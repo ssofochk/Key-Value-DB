a01a86f8684381ab1c048bb1e55ea7d837007a01
Баев Артур <arturbaev4545@gmail.com>
2026-06-22 20:06:06 +0300

    seq loop, uniform, zipf implemented with benchmark_runner

---
+++ benchmarks/sequential_loop_bm.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+class SeqLoopWorkLoad {
+private:
+    size_t cur_idx = 0;
+public:
+    template <Policies Policy>
+    void execute(Cache<Policy, int>& cache, const std::vector<std::string>& keys) {
+        cache.Get(keys[cur_idx % keys.size()]);
+        ++cur_idx;
+    }
+};
+
+using Cache_NoEviction = CacheFixture<Policies::NOEVICTION>;
+BENCHMARK_F(Cache_NoEviction, SeqLoop) 
+(benchmark::State& state) {
+    bm_impl<Policies::NOEVICTION, SeqLoopWorkLoad>(state, cache, keys, SeqLoopWorkLoad{});
+}
+
+using Cache_Random = CacheFixture<Policies::RANDOM>;
+BENCHMARK_F(Cache_Random, SeqLoop) 
+(benchmark::State& state) {
+    bm_impl<Policies::RANDOM, SeqLoopWorkLoad>(state, cache, keys, SeqLoopWorkLoad{});
+}
+
+
+using Cache_LRU = CacheFixture<Policies::LRU>;
+BENCHMARK_F(Cache_LRU, SeqLoop) 
+(benchmark::State& state) {
+    bm_impl<Policies::LRU, SeqLoopWorkLoad>(state, cache, keys, SeqLoopWorkLoad{});
+}
+
+using Cache_LFU = CacheFixture<Policies::LFU>;
+BENCHMARK_F(Cache_LFU, SeqLoop) 
+(benchmark::State& state) {
+    bm_impl<Policies::LFU, SeqLoopWorkLoad>(state, cache, keys, SeqLoopWorkLoad{});
+}
