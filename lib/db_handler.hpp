0499a384bb92c93f6894c016a0827b5806c5a423
LamaDevGuy <86229883+LamaDevGuy@users.noreply.github.com>
2026-06-22 20:39:14 +0300

    gave wrapper ability to specify amount of mutexes and their type in template params

--- lib/db_handler.hpp
+++ lib/db_handler.hpp
@@ -4,30 +4,37 @@
 #include <future>
 #include <memory>
 #include <type_traits>
+#include <new>
+#include <shared_mutex>
 
 namespace db_handler{
 
 template<typename T>
 concept IsFilePath = std::is_same_v<std::decay_t<T>, std::filesystem::path>;
 
-template <int ThreadCount, int MutexCount, IsFilePath FileWorkerPath, Policies Policy, typename... Types>
+template <int ThreadCount, int MutexCount, IsFilePath auto FileWorkerPath, Policies Policy, bool IsMutexShared = false>
 class Handler{
 public:
 
-    using Variant = Cache<Policy, Types...>::Variant;
-    using Element = Cache<Policy, Types...>::Element;
+    std::future<std::optional<std::string>> Get(const std::string& key);
 
-
-    std::future<Variant*> Get(const std::string& key);
-
-    std::future<bool> Set(const std::string& key, Variant value);
+    std::future<bool> Set(const std::string& key, const std::string& value);
 
 private:
     ThreadPool thread_pool_ = ThreadPool(ThreadCount);
-    // FileWorker io_worker_ = FileWorker(FileWorkerPath);
-    Cache<Policy, Types...> cache_module_ = Cache<Policy, Types...>();
-    std::mutex cache_mutex_;                                                    // temporarily one mutex, later could be  fragmentation
-    // std::mutex fragments_mutexes[MutexCount];                                // commented for now, will test on later versions
+    FileWorker io_worker_ = FileWorker(FileWorkerPath);
+    Cache<Policy> cache_module_ = Cache<Policy>();
+
+    using ChosenMutex = std::conditional_t<IsMutexShared, std::shared_mutex, std::mutex>;
+
+    struct alignas(std::hardware_destructive_interference_size) AlignedMutex {
+        ChosenMutex mtx;
+    };
+    AlignedMutex fragments_mutexes[MutexCount];
+
+    std::hash<std::string> hash_func_;
+
+    size_t GetMutexHash(const std::string& key);
 };
 
 }; // namespace db_handler
